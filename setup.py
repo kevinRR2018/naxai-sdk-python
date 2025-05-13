cf537ee6020c4304537c32becd22220be690a7b1
Kevin <kevin@ringring.be>
2025-05-13 14:22:23 +0200

    version 0.0.16 Force pydantic version to 2.0 or higher

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='naxai',
     packages=find_packages(),
     description='Python sdk for Naxai\'s API',
-    version='0.0.15',
+    version='0.0.16',
     url='https://github.com/kevinRR2018/naxai-sdk-python',
     author='kevin',
     author_email='k.bertin@ringring.be',
     keywords=['voice','naxai','sdk', "sms", "email", "communication"],
-    install_requires=['pydantic',
+    install_requires=['pydantic>=2.0',
                       'httpx'],
     )
