149b9cdea4419623d66399db5654ec3e1846c285
Kevin <kevin@ringring.be>
2025-05-14 08:21:58 +0200

    Added new samples. + adapt existing samples

--- samples/sms/async/send_message.py
+++ samples/sms/async/send_message.py
@@ -2,15 +2,17 @@ import asyncio
 from naxai.async_client import NaxaiAsyncClient
 from naxai.models.sms.responses.send_responses import SendSMSResponse
 
-DESTINATION = "123456789"
-FROM_NUMBER = "8810"
+# Assuming NAXAI_CLIENT_ID and NAXAI_SECRET are set in environment.
+
+DESTINATION = "destination_of_the_sms"
+FROM_NUMBER = "originating_number_of_sms"
 
 async def main():
 
     async with NaxaiAsyncClient() as client:
         response: SendSMSResponse = await client.sms.send(to=[DESTINATION],
                                                           from_=FROM_NUMBER,
-                                                          body="This is a test message from Naxai SDK!") 
+                                                          body="This is a test message from Naxai SDK!")
         for message in response.messages:
             print(f"Message with id: {message.message_id} to destination: {message.to} will be sent shortly.")
 
