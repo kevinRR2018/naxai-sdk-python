e09b826bdaba7cfb2a8f535da2be53f40c9e290e
Kevin <kevin@ringring.be>
2025-05-16 08:38:12 +0200

    Removal of people sub resources:  - imports  - exports

--- naxai/resources_async/people.py
+++ naxai/resources_async/people.py
@@ -1,7 +1,5 @@
 from .people_resources.attributes import AttributesResource
 from .people_resources.contacts import ContactsResource
-from .people_resources.exports import ExportsResource
-from .people_resources.imports import ImportsResource
 from .people_resources.segments import SegmentsResource
 
 class PeopleResource:
@@ -14,6 +12,4 @@ class PeopleResource:
         self.root_path = "/people"
         self.attributes: AttributesResource = AttributesResource(self._client, self.root_path)
         self.contacts: ContactsResource = ContactsResource(self._client, self.root_path)
-        self.exports: ExportsResource = ExportsResource(self._client, self.root_path)
-        self.imports: ImportsResource = ImportsResource(self._client, self.root_path)
         self.segments: SegmentsResource = SegmentsResource(self._client, self.root_path)
--- tests/unit/resources/people/test_people_resource_sync.py
+++ tests/unit/resources/people/test_people_resource_sync.py
@@ -6,8 +6,6 @@ from unittest.mock import patch, MagicMock
 from naxai.resources.people import PeopleResource
 from naxai.resources.people_resources.attributes import AttributesResource
 from naxai.resources.people_resources.contacts import ContactsResource
-from naxai.resources.people_resources.exports import ExportsResource
-from naxai.resources.people_resources.imports import ImportsResource
 from naxai.resources.people_resources.segments import SegmentsResource
 
 
@@ -33,20 +31,14 @@ class TestPeopleResourceSync:
         # Test that all sub-resources are initialized correctly
         assert isinstance(people_resource.attributes, AttributesResource)
         assert isinstance(people_resource.contacts, ContactsResource)
-        assert isinstance(people_resource.exports, ExportsResource)
-        assert isinstance(people_resource.imports, ImportsResource)
         assert isinstance(people_resource.segments, SegmentsResource)
         
         # Test that all sub-resources have the correct root path
         assert people_resource.attributes.root_path == "/people/attributes"
         assert people_resource.contacts.root_path == "/people/contacts"
-        assert people_resource.exports.root_path == "/people/exports"
-        assert people_resource.imports.root_path == "/people/imports"
         assert people_resource.segments.root_path == "/people/segments"
         
         # Test that all sub-resources have the correct client
         assert people_resource.attributes._client == mock_client
         assert people_resource.contacts._client == mock_client
-        assert people_resource.exports._client == mock_client
-        assert people_resource.imports._client == mock_client
         assert people_resource.segments._client == mock_client
